b4d8c099bcbb0fbe5c9b0cdf2174043026ec9f36
banflam <rishiagrawal2002@gmail.com>
2025-07-12 10:46:47 -0400

    define the receipts table

--- agent.py
+++ agent.py
@@ -20,9 +20,17 @@ metadata_obj = MetaData()
 def insert_rows_into_table(rows, table, engine=engine):
 
     for row in rows:
-        stmt = insert(table).values(**row)
+        stmt = insert(table).values(**row) # standard SQL statement expressed programmatically
         with engine.begin() as connection:
             connection.execute(stmt)
             
 
-    
+table_name = "receipts"
+receipts = Table(
+    table_name,
+    metadata_obj,
+    Column("receipt_id", Integer, primary_key=True),
+    Column("customer_name", String(16), primary_key=True),
+    Column("price", Float),
+    Column("tip", Float),
+)    
